0e0b135d221f98ac8dca48e6f6d7939301158a88
Prathibha Gunathilaka <prathibhagunathilaka@gmail.com>
2026-04-09 20:40:23 +0530

    Add ResourceStatus and ResourceType enums; implement ResourceRepository for resource management

---
+++ backend/src/main/java/com/smartcampus/entity/enums/ResourceStatus.java
@@ -0,0 +1,8 @@
+package com.smartcampus.entity.enums;
+
+public enum ResourceStatus {
+    WORKING,
+    OUT_OF_SERVICE,
+    UNDER_MAINTENANCE,
+    DELETED
+}
---
+++ backend/src/main/java/com/smartcampus/entity/enums/ResourceType.java
@@ -0,0 +1,10 @@
+package com.smartcampus.entity.enums;
+
+public enum ResourceType {
+    LECTURE_HALL,
+    COMPUTER_LAB,
+    MEETING_ROOM,
+    EQUIPMENT,
+    AUDITORIUM,
+    SMART_CLASSROOM
+}
